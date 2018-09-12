8b36d19da2d59dd15ecf1226aa84353045f4e141
Muhammed Saleem P <muhammedsalimp@gmail.com>
2018-09-12 11:55:52 +0530

    Add files via upload

---
+++ EFCoreCodeFirstSample/Models/Repository/IDataRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace EFCoreCodeFirstSample.Models.Repository
+{
+    public interface IDataRepository<TEntity>
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity Get(long id);
+        int Add(TEntity entity);
+        int Update(long id, TEntity entity);
+        int Delete(long id);
+    }
+}
